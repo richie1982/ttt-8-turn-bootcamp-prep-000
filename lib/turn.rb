531095e828cb3608b19b58c4e67f1d5e00e24b39
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-10 12:10:41 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,18 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+ def valid_move?(board, position)
+  if position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+      true
+    end
+  end
+
+def turn(board)
+  puts "Please enter 1-9: ";
+end
+  
